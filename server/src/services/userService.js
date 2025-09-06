baaac1a052713546c7f8e1b630c33884067d17e4
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-06 17:29:28 +0300

    add user service

---
+++ server/src/services/userService.js
@@ -0,0 +1,47 @@
+import User from "../Models/User.js";
+import userUtils from "../utils/userUtils.js";
+import bcrypt from "bcrypt";
+
+export default {
+    async register(userData) {
+        const existingEmail = User.find({ email: userData.email });
+
+        if (existingEmail) {
+            throw new Error("Email already exists!");
+        }
+
+        const createdUser = await User.create(userData);
+
+        if (!createdUser) {
+            throw new Error("Invalid User!");
+        }
+
+        const token = userUtils.getToken(createdUser);
+
+        return {
+            _id: createdUser.id,
+            email: createdUser.email,
+            accessToken: token,
+        };
+    },
+    async login(userData) {
+        const user = await User.find({ email: userData.email });
+
+        if (!user) {
+            throw new Error("Invalid Email or Password!");
+        }
+        const isValid = await bcrypt.compare(userData.password, user.password);
+
+        if (!isValid) {
+            throw new Error("Invalid Email or Password!");
+        }
+
+        const token = userUtils.getToken(user);
+
+        return {
+            _id: createdUser.id,
+            email: createdUser.email,
+            accessToken: token,
+        };
+    },
+};
