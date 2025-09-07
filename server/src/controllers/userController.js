ae8b61d265a52f5f02ffd7eea63f37f53597fdbb
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-07 11:50:00 +0300

    add user error handling

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,17 +6,26 @@ const userController = Router();
 userController.post("/register", async (req, res) => {
     const userData = req.body;
 
-    const result = await userService.register(userData);
+    try {
+        const result = await userService.register(userData);
 
-    res.json(result);
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: err.message });
+    }
 });
 
 userController.post("/login", async (req, res) => {
     const userData = req.body;
+    try {
+        const result = await userService.login(userData);
 
-    const result = await userService.login(userData);
-
-    res.json(result);
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: err.message });
+    }
 });
 
 userController.get("/logout", async (req, res) => {
